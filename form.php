54cc7fe6f5bd77f3f4a0b7b476bf0b6d9c81d866
artred02 <88457259+artred02@users.noreply.github.com>
2023-05-16 11:10:55 +0200

    test db 15

--- form.php
+++ form.php
@@ -17,7 +17,7 @@ try {
             } catch (Exception $e) {
                 echo $e;
             }
-            header("Location: /");
+            header("Location: /index.php");
         }
     }
 } catch (Exception $e) {
